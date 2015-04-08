64a8375e96bc94693696b7ec4cb426dc9625d783
Adam Jowett <adam@mettro.com.au>
2015-04-08 15:36:54 +1000

    Self initiazing function, added .gitignore and .gitattributes

--- clickoutside.directive.js
+++ clickoutside.directive.js
@@ -1,38 +1,40 @@
-angular
-    .module('mouse.utils', [])
-    .directive('clickOutside', ['$document', clickOutside]);
+(function () {
+    angular
+        .module('mouse.utils', [])
+        .directive('clickOutside', ['$document', clickOutside]);
 
-function clickOutside($document) {
-    return {
-        restrict: 'A',
-        scope: {
-            clickOutside: '&'
-        },
-        link: function ($scope, elem, attr) {
-            var classList = (attr.outsideIfNot !== undefined) ? attr.outsideIfNot.replace(', ', ',').split(',') : [];
-            if (attr.id !== undefined) classList.push(attr.id);
+    function clickOutside($document) {
+        return {
+            restrict: 'A',
+            scope: {
+                clickOutside: '&'
+            },
+            link: function ($scope, elem, attr) {
+                var classList = (attr.outsideIfNot !== undefined) ? attr.outsideIfNot.replace(', ', ',').split(',') : [];
+                if (attr.id !== undefined) classList.push(attr.id);
 
-            $document.on('click', function (e) {
-                var i = 0,
-                    element;
+                $document.on('click', function (e) {
+                    var i = 0,
+                        element;
 
-                if (!e.target) return;
+                    if (!e.target) return;
 
-                for (element = e.target; element; element = element.parentNode) {
-                    var id = element.id;
-                    var classNames = element.className;
+                    for (element = e.target; element; element = element.parentNode) {
+                        var id = element.id;
+                        var classNames = element.className;
 
-                    if (id !== undefined) {
-                        for (i = 0; i < classList.length; i++) {
-                            if (id.indexOf(classList[i]) > -1 || classNames.indexOf(classList[i]) > -1) {
-                                return;
+                        if (id !== undefined) {
+                            for (i = 0; i < classList.length; i++) {
+                                if (id.indexOf(classList[i]) > -1 || classNames.indexOf(classList[i]) > -1) {
+                                    return;
+                                }
                             }
                         }
                     }
-                }
 
-                $scope.$eval($scope.clickOutside);
-            });
-        }
-    };
-}
+                    $scope.$eval($scope.clickOutside);
+                });
+            }
+        };
+    }
+})();
