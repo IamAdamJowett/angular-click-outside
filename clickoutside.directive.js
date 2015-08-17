c61b0b95652ce069246129a77396c065d8743a49
Adam Jowett <akuma.me@gmail.com>
2015-08-17 20:31:35 +1000

    Cleaned up the click handler when scope is destroyed

--- clickoutside.directive.js
+++ clickoutside.directive.js
@@ -8,30 +8,47 @@ function clickOutside($document) {
         scope: {
             clickOutside: '&'
         },
-        link: function ($scope, elem, attr) {
+        link: function($scope, elem, attr) {
             var classList = (attr.outsideIfNot !== undefined) ? attr.outsideIfNot.replace(', ', ',').split(',') : [];
-            if (attr.id !== undefined) classList.push(attr.id);
 
-            $document.on('click', function (e) {
+            // add the elements id so it is not counted in the click listening
+            if (attr.id !== undefined) {
+                classList.push(attr.id);
+            }
+
+            // assign the document click handler to a variable so we can un-register it when the directive is destroyed
+            var clickHandler = $document.on('click', function(e) {
                 var i = 0,
                     element;
 
-                if (!e.target) return;
+                // if there is no click target, no point going on
+                if (!e || !e.target) {
+                    return;
+                }
 
+                // loop through the available elements, looking for classes in the class list that might match and so will eat
                 for (element = e.target; element; element = element.parentNode) {
-                    var id = element.id;
-                    var classNames = element.className;
+                    var id = element.id,
+                        classNames = element.className,
+                        l = classList.length;
 
-                    if (id !== undefined) {
-                        for (i = 0; i < classList.length; i++) {
-                            if (id.indexOf(classList[i]) > -1 || classNames.indexOf(classList[i]) > -1) {
-                                return;
-                            }
+                    // loop through the elements id's and classnames looking for exceptions
+                    for (i = 0; i < l; i++) {
+                        // check for id's or classes, but only if they exist in the first place
+                        if ((id !== undefined && id.indexOf(classList[i]) > -1) || (classNames.length && classNames.indexOf(classList[i]) > -1)) {
+                            // now let's exit out as it is an element that has been defined as being ignored for clicking outside
+                            return;
                         }
                     }
                 }
 
+                // if we have got this far, then we are good to go with processing the command passed in via the click-outside attribute
                 $scope.$eval($scope.clickOutside);
+            });
+
+            // when the scope is destroyed, clean up the documents click handler as we don't want it hanging around
+            $scope.$on('$destroy', function() {
+                clickHandler();
             });
         }
     };
