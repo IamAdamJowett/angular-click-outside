005525c582f6ddf19ff02eaddf4d74473dca31f0
Anatejms <anatejms@gmail.com>
2016-02-20 14:19:05 +0100

    Update clickoutside.directive.js
    Helpful for automatic unique ID generation, you can use one time binding inside dropdown directive to handle all ids
    
    eq.
    directive
        .directive('dropdown', function() {
            var uniqueId = 1;
        return {
            restrict: 'E',
            link: function(scope, element, attrs) {
                scope.uniqueId = 'hsSelect' + uniqueId++;
                scoep.close = function(){...}
            }
        };
    });
    
    and then in template:
    
    id="{{::uniqueId}}" click-outside="close()"
    
    without timeout directive gets empty id

--- clickoutside.directive.js
+++ clickoutside.directive.js
@@ -5,80 +5,83 @@
 
     angular
         .module('angular-click-outside', [])
-        .directive('clickOutside', ['$document', '$parse', clickOutside]);
+        .directive('clickOutside', ['$document', '$parse', '$timeout', clickOutside]);
 
-    function clickOutside($document, $parse) {
+    function clickOutside($document, $parse, $timeout) {
         return {
             restrict: 'A',
             link: function($scope, elem, attr) {
-                var classList = (attr.outsideIfNot !== undefined) ? attr.outsideIfNot.replace(', ', ',').split(',') : [],
-                    fn;
+                // postpone linking for unique id generation
+                $timeout(function(){
+                    var classList = (attr.outsideIfNot !== undefined) ? attr.outsideIfNot.replace(', ', ',').split(',') : [],
+                        fn;
 
-                // add the elements id so it is not counted in the click listening
-                if (attr.id !== undefined) {
-                    classList.push(attr.id);
-                }
-
-                function eventHandler(e) {
-
-                    // check if our element already hidden and abort if so
-                    if (angular.element(elem).hasClass("ng-hide")) {
-                        return;
+                    // add the elements id so it is not counted in the click listening
+                    if (attr.id !== undefined) {
+                        classList.push(attr.id);
                     }
 
-                    var i = 0,
-                        element;
+                    function eventHandler(e) {
 
-                    // if there is no click target, no point going on
-                    if (!e || !e.target) {
-                        return;
-                    }
-
-                    // loop through the available elements, looking for classes in the class list that might match and so will eat
-                    for (element = e.target; element; element = element.parentNode) {
-                        var id = element.id,
-                            classNames = element.className,
-                            l = classList.length;
-
-                        // Unwrap SVGAnimatedString
-                        if (classNames && classNames.baseVal !== undefined) {
-                            classNames = classNames.baseVal;
+                        // check if our element already hidden and abort if so
+                        if (angular.element(elem).hasClass("ng-hide")) {
+                            return;
                         }
 
-                        // loop through the elements id's and classnames looking for exceptions
-                        for (i = 0; i < l; i++) {
-                            // check for id's or classes, but only if they exist in the first place
-                            if ((id !== undefined && id === classList[i]) || (classNames && classNames === classList[i])) {
-                                // now let's exit out as it is an element that has been defined as being ignored for clicking outside
-                                return;
+                        var i = 0,
+                            element;
+
+                        // if there is no click target, no point going on
+                        if (!e || !e.target) {
+                            return;
+                        }
+
+                        // loop through the available elements, looking for classes in the class list that might match and so will eat
+                        for (element = e.target; element; element = element.parentNode) {
+                            var id = element.id,
+                                classNames = element.className,
+                                l = classList.length;
+
+                            // Unwrap SVGAnimatedString
+                            if (classNames && classNames.baseVal !== undefined) {
+                                classNames = classNames.baseVal;
+                            }
+
+                            // loop through the elements id's and classnames looking for exceptions
+                            for (i = 0; i < l; i++) {
+                                // check for id's or classes, but only if they exist in the first place
+                                if ((id !== undefined && id === classList[i]) || (classNames && classNames === classList[i])) {
+                                    // now let's exit out as it is an element that has been defined as being ignored for clicking outside
+                                    return;
+                                }
                             }
                         }
+
+                        // if we have got this far, then we are good to go with processing the command passed in via the click-outside attribute
+                        return $scope.$apply(function() {
+                            fn = $parse(attr['clickOutside']);
+                            return fn($scope);
+                        });
                     }
 
-                    // if we have got this far, then we are good to go with processing the command passed in via the click-outside attribute
-                    return $scope.$apply(function() {
-                        fn = $parse(attr['clickOutside']);
-                        return fn($scope);
+                    // detect if touch device and listen to touchstart instead of click
+                    var ua = navigator.userAgent,
+                        event = (hasTouch()) ? "touchstart" : "click";
+
+                    // assign the document click handler to a variable so we can un-register it when the directive is destroyed
+                    $document.on(event, eventHandler);
+
+                    // when the scope is destroyed, clean up the documents click handler as we don't want it hanging around
+                    $scope.$on('$destroy', function() {
+                        $document.off('click', eventHandler);
                     });
-                }
 
-                // detect if touch device and listen to touchstart instead of click
-                var ua = navigator.userAgent,
-                    event = (hasTouch()) ? "touchstart" : "click";
-
-                // assign the document click handler to a variable so we can un-register it when the directive is destroyed
-                $document.on(event, eventHandler);
-
-                // when the scope is destroyed, clean up the documents click handler as we don't want it hanging around
-                $scope.$on('$destroy', function() {
-                    $document.off('click', eventHandler);
-                });
-                
-                // attempt to figure out if we are on a touch device
-                function hasTouch() {
-                    // works on most browsers, IE10/11 and Surface
-                    return 'ontouchstart' in window || navigator.maxTouchPoints;
-                };
+                    // attempt to figure out if we are on a touch device
+                    function hasTouch() {
+                        // works on most browsers, IE10/11 and Surface
+                        return 'ontouchstart' in window || navigator.maxTouchPoints;
+                    };
+                }, 0)
             }
         };
     }
