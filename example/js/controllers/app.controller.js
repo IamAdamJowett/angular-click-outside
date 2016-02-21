e25a7fdd53105f3405f9c669b112eba5dfc1d35e
Adam Jowett <akuma.me@gmail.com>
2016-02-21 04:11:42 +0000

    streamlined example to be based around dropdown menu

--- example/js/controllers/app.controller.js
+++ example/js/controllers/app.controller.js
@@ -6,16 +6,10 @@
         .controller('AppController', ['$scope', '$document', AppController]);
 
     function AppController($scope, $document) {
-        $scope.feedback = '';
         $scope.closeThis = closeThis;
         
         function closeThis() {
             console.log('clicked outside');
-            $scope.feedback = 'clicked outside';
         }
-        
-        $document.on('mousedown', function (e) {
-            $scope.feedback = '';
-        });
     }
 })();
