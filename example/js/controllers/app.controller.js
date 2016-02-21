695b7f797cb06ed92c65caa596f35919cae37d62
Adam Jowett <akuma.me@gmail.com>
2016-02-21 04:01:00 +0000

    Updated example code

--- example/js/controllers/app.controller.js
+++ example/js/controllers/app.controller.js
@@ -3,13 +3,19 @@
     'use strict';
 
     angular.module('exampleApp')
-        .controller('AppController', AppController);
+        .controller('AppController', ['$scope', '$document', AppController]);
 
-    function AppController($scope) {
+    function AppController($scope, $document) {
+        $scope.feedback = '';
         $scope.closeThis = closeThis;
         
         function closeThis() {
             console.log('clicked outside');
+            $scope.feedback = 'clicked outside';
         }
+        
+        $document.on('mousedown', function (e) {
+            $scope.feedback = '';
+        });
     }
 })();
